38bc889838bf14d9e515ade570dba82014e6613d
atul-chakrawarti <atulchakrawarti@gmail.com>
2026-05-15 15:31:42 +0530

    feat: add add() and subtract() calculator functions

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
 print('My Assignment for Github')
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+print(add(10, 5))       # Output: 15
+print(subtract(10, 5))  # Output: 5
